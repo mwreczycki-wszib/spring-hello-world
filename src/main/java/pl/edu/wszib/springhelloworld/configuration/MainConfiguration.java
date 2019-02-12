83ebccc418ed64c0b5bb2c12a55e924cf772df91
Mateusz Wręczycki <setsudan.hana@gmail.com>
2019-02-12 23:43:22 +0100

    add solution

--- src/main/java/pl/edu/wszib/springhelloworld/configuration/MainConfiguration.java
+++ src/main/java/pl/edu/wszib/springhelloworld/configuration/MainConfiguration.java
@@ -2,13 +2,33 @@ package pl.edu.wszib.springhelloworld.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import pl.edu.wszib.springhelloworld.model.EkspresDoKawy;
 import pl.edu.wszib.springhelloworld.model.Komputer;
 import pl.edu.wszib.springhelloworld.model.Kubek;
 import pl.edu.wszib.springhelloworld.model.Programista;
 
+import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
+
 @Configuration
 public class MainConfiguration {
+
+    @Bean
+    @Scope(SCOPE_PROTOTYPE)
+    public Kubek kubek() {
+        return new Kubek();
+    }
+
+    @Bean
+    @Scope(SCOPE_PROTOTYPE)
+    public Komputer komputer() {
+        return new Komputer();
+    }
+
+    @Bean
+    public EkspresDoKawy ekspresDoKawy() {
+        return new EkspresDoKawy();
+    }
 
     @Bean
     public Programista marek(Kubek kubek, Komputer komputer, EkspresDoKawy ekspresDoKawy) {
